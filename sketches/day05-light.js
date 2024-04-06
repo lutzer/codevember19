914006769b8c20ce2ecaca6288134cf3be90729b
Lutz Reiter <lutz@neu.io>
2024-04-06 19:47:01 +0200

    made changes on light shader

--- sketches/day05-light.js
+++ sketches/day05-light.js
@@ -16,6 +16,7 @@ require('three/examples/js/shaders/LuminosityHighPassShader.js');
 const PIXEL_BUFFER_SIZE = 6;
 const CAMERA_RADIUS = 2.5;
 
+
 // Setup our sketch
 const settings = {
   dimensions: [ 512, 512 ],
@@ -27,7 +28,7 @@ const flameShader = {
   uniforms: {
     // Expose props from canvas-sketch
     time: { value: 1.0 },
-    grid: { value : 16.0 },
+    grid: { value : 12.0 },
     xFreq: { value: 1.8 },
     yFreq: { value: 1.0 },
     seed : { value : 0.0 },
@@ -42,18 +43,21 @@ const flameShader = {
     uniform float hue;
     uniform float xFreq;
     uniform float yFreq;
+
     varying vec2 vUv;
+    varying vec4 worldPosition;
+    varying vec3 localPosition;
 
     #pragma glslify: noise = require('glsl-noise/simplex/3d');
     #pragma glslify: hsv2rgb = require(glsl-y-hsv/hsv2rgb) 
 
     void main () {
-      vec2 grid = floor(vUv.xy * grid) / grid;
+      vec2 grid = floor(localPosition.xy * grid) / grid;
       float n = noise(vec3(grid.x * xFreq, grid.y * yFreq - time * 1.0, time * 0.8 + seed));
 
       float prob = min(log(grid.y) / log(.05),1.0);
 
-      float brightness = (prob * n) > 0.1 ? abs(prob * n) : 0.0;
+      float brightness = (prob * n) > 0.1 ? prob * n : 0.0;
 
       // vec3 hsv = vec3(hue + brightness * 0.1, 1.0, brightness);
       vec3 hsv = vec3(hue + brightness * 0.15, 1.0, 1.0);
@@ -65,8 +69,13 @@ const flameShader = {
   `),
   vertexShader: glslify(/* glsl */`
     varying vec2 vUv;
+    varying vec4 worldPosition;
+    varying vec3 localPosition;
+
     void main () {
       vUv = uv;
+      localPosition = position.xyz;
+      worldPosition = modelViewMatrix * vec4(position.xyz, 1.0);
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xyz, 1.0);
     }
   `)
@@ -87,13 +96,18 @@ function randomWalk(pos, speed, dt) {
 // Your sketch, which simply returns the shader
 const sketch = ({ context, width, height }) => {
 
+  const gridSize = random.rangeFloor(6,14);
+  const voxelSize = random.range(0.01,0.25);
+  const hueRandomness = random.range(1,30);
+  const startAngle = random.range(0, Math.PI*2);
+
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
     canvas: context.canvas
   });
 
   // WebGL background color
-  renderer.setClearColor("#000", 1.0);
+  renderer.setClearColor("#fff", 1.0);
 
   // Setup a camera
   const camera = new THREE.PerspectiveCamera(42, 1, 0.01, 100);
@@ -102,17 +116,19 @@ const sketch = ({ context, width, height }) => {
   const scene = new THREE.Scene();
 
   // add flames
-  const flames = _.range(-0.7, 0.7, 0.01).map( (val, index) => {
+  const flames = _.range(-0.5, 0.5, voxelSize).map( (val, index) => {
     const flame = new THREE.Mesh(
-      new THREE.PlaneGeometry(1,1),
+      new THREE.BoxBufferGeometry(1, 1, voxelSize*0.99),
       new THREE.ShaderMaterial(_.cloneDeep(flameShader))
     );
     flame.material.transparent = true
     flame.material.side = THREE.DoubleSide
+
     flame.position.setZ(val)
     flame.position.setY(0.25)
     setShaderUniforms(flame.material, { 
-      seed: index
+      seed: index,
+      grid: gridSize
     })
     scene.add(flame);
     return flame
@@ -140,13 +156,13 @@ const sketch = ({ context, width, height }) => {
     },
     render ({time}) {
 
-      var angle = time * 0.1;
+      var angle = startAngle + time * 0.2;
 
-      camera.position.set(Math.sin(angle)*CAMERA_RADIUS, 1.2, Math.cos(angle)*CAMERA_RADIUS);
-      camera.lookAt(new THREE.Vector3(0,0,0));
+      camera.position.set(Math.sin(angle)*CAMERA_RADIUS, 0.9, Math.cos(angle)*CAMERA_RADIUS);
+      camera.lookAt(new THREE.Vector3(0,0.15,0));
 
       flames.forEach( (flame, index) => {
-        setShaderUniforms(flame.material, { time: time * 0.5, hue : time/30.0 + index * 0.015 })
+        setShaderUniforms(flame.material, { time: time * 0.5, hue : time/20.0 + index / hueRandomness })
       })
 
       composer.render()
